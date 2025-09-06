8661286d454813175a18ebc618579d3421fa657d
Flaviojcf <flaviojcostafilho@gmail.com>
2025-09-06 18:41:34 -0300

    feat(ci): enhance CI pipeline with SonarCloud integration

---
+++ src/FCG.Domain/Sonar.cs
@@ -0,0 +1,12 @@
+﻿namespace FCG.Domain
+{
+    public class Sonar
+    {
+        public Sonar(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; } = string.Empty;
+    }
+}
