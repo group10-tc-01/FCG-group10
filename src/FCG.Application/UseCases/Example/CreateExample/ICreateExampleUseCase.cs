5e69f33e33d5583ceb32fcf20c8a4c4de1b2ff51
Flaviojcf <flaviojcostafilho@gmail.com>
2025-09-14 22:37:32 -0300

    feat(tests): add FCG.FunctionalTests project for testing

--- src/FCG.Application/UseCases/Example/CreateExample/ICreateExampleUseCase.cs
+++ src/FCG.Application/UseCases/Example/CreateExample/ICreateExampleUseCase.cs
@@ -1,8 +1,6 @@
 ﻿using MediatR;
-using System.Diagnostics.CodeAnalysis;
 
 namespace FCG.Application.UseCases.Example.CreateExample
 {
-    [ExcludeFromCodeCoverage(Justification = "Example code, will be removed")]
     public interface ICreateExampleUseCase : IRequestHandler<CreateExampleInput, CreateExampleOutput> { }
 }
