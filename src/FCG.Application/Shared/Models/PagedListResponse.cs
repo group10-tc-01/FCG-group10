695eed40bd274076c14df953bce3b5d68b9befc7
Rhuaannn <106773136+rhuaannn@users.noreply.github.com>
2025-10-14 14:35:39 -0300

    Update PagedListResponse.cs
    Remove coments in class

--- src/FCG.Application/Shared/Models/PagedListResponse.cs
+++ src/FCG.Application/Shared/Models/PagedListResponse.cs
@@ -1,25 +1,3 @@
-﻿//namespace FCG.Application.Shared.Models
-//{
-//    public sealed class PagedListResponse<T>
-//    {
-//        public int CurrentPage { get; set; }
-//        public int TotalPages { get; set; }
-//        public int PageSize { get; set; }
-//        public int TotalCount { get; set; }
-//        public bool HasPrevious => CurrentPage > 1;
-//        public bool HasNext => CurrentPage < TotalPages;
-//        public List<T> Items { get; set; } = new List<T>();
-//        public PagedListResponse(List<T> items, int count, int pageNumber, int pageSize)
-//        {
-//            TotalCount = count;
-//            PageSize = pageSize;
-//            CurrentPage = pageNumber;
-//            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
-//            Items.AddRange(items);
-
-//        }
-//    }
-//}
 namespace FCG.Application.Shared.Models
 {
     public sealed class PagedListResponse<T>
@@ -32,12 +10,11 @@ namespace FCG.Application.Shared.Models
         public bool HasNext => CurrentPage < TotalPages;
         public List<T> Items { get; set; } = new List<T>();
 
-        // 👇 A CORREÇÃO ESTÁ AQUI 👇
         public PagedListResponse(List<T> items, int totalCount, int currentPage, int pageSize)
         {
             TotalCount = totalCount;
             PageSize = pageSize;
-            CurrentPage = currentPage; // Agora os nomes batem!
+            CurrentPage = currentPage;
             TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
             Items.AddRange(items);
         }
