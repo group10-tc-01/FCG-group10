7dff7ed6f13aca6b6224fc67448b0bd79fa3b4df
Flávio Costa <flaviojcostafilho@gmail.com>
2025-10-19 19:45:03 -0300

    chore(config): exclude configuration classes from code coverage

--- src/FCG.Infrastructure/Persistance/Configuration/GameConfiguration.cs
+++ src/FCG.Infrastructure/Persistance/Configuration/GameConfiguration.cs
@@ -1,9 +1,11 @@
 ﻿using FCG.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Diagnostics.CodeAnalysis;
 
 namespace FCG.Infrastructure.Persistance.Configuration
 {
+    [ExcludeFromCodeCoverage]
     public class GameConfiguration : BaseConfiguration<Game>
     {
         public override void Configure(EntityTypeBuilder<Game> builder)
